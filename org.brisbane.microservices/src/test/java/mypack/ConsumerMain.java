6f463a94cd11a90b96a84edd9733f3c89211179b
mutha <mutha@192.168.1.7>
2020-08-11 12:35:32 +1000

    first commit msg

---
+++ org.brisbane.microservices/src/test/java/mypack/ConsumerMain.java
@@ -0,0 +1,48 @@
+package mypack;
+
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import au.com.dius.pact.provider.junit.loader.PactFolder;
+
+import au.com.dius.pact.provider.junit.PactRunner;
+import au.com.dius.pact.provider.junit.Provider;
+import au.com.dius.pact.provider.junit.State;
+import au.com.dius.pact.provider.junit.target.HttpTarget;
+import au.com.dius.pact.provider.junit.target.Target;
+import au.com.dius.pact.provider.junit.target.TestTarget;
+
+@RunWith(PactRunner.class)
+@Provider("infoprovider")
+@PactFolder("tatget\\pacts")
+
+public class ConsumerMain
+{
+	private static final int WIREMOCK_PORT=9090;
+	@BeforeClass
+	public static void setup()
+	{
+		WireMockServer wireMockServer;
+		wireMockServer=new WireMockServer(WIREMOCK_PORT);
+		wireMockServer.start();
+		wireMockServer.stubFor(get(urlEqualTo("/info"))
+				.willReturn(aResponse().withStatus(200)
+				.withHeader("Content-Type","application/json")
+				.withBody("{\"id\":\"11\",\"name\":\"kalam\",\"age\":\"78\"}")
+				));
+				
+	}
+	
+	@TestTarget
+	public final Target target =new HttpTarget("http","localhost",WIREMOCK_PORT);
+	@State("test GET")
+	public void toGetState()
+	{
+	}
+	
+	}
+
+	
+	
---
+++ org.brisbane.microservices/src/test/java/mypack/Consumerpact3.java
@@ -0,0 +1,47 @@
+
+package mypack;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.springframework.web.client.RestTemplate;
+
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.consumer.junit.PactVerification;
+import au.com.dius.pact.core.model.annotations.Pact;
+import au.com.dius.pact.model.RequestResponsePact;
+import org.springframework.http.ResponseEntity;
+import au.com.dius.pact.consumer.PactProviderRuleMk2;
+
+public class Consumerpact3
+{
+	@Rule
+	public PactProviderRuleMk2 mkprovider=new PactProviderRuleMk2("infoprovider","localhost",9090,this);
+	@Pact(consumer="consumer_C") public RequestResponsePact createPact(PactDslWithProvider builder)
+	
+	{
+		System.out.println(mkprovider);
+		Map<String,String> headers=new HashMap<>();
+		headers.put("Content-Type","application/json");
+		return(builder.given("test GET")
+				.uponReceiving("GET REQUEST")
+				.path("/info").method("GET")
+				.willRespondWith()
+				.status(200)
+				.headers(headers)
+				.body("{\"id\":\"11\",\"name\":\"kalam\",\"age\":\"78\"}")
+				.toPact());
+	}
+	@Test
+	@PactVerification
+	public void testconsumercontract3()
+	{
+		RestTemplate rt=new RestTemplate();
+		ResponseEntity<String> res=rt.getForEntity(mkprovider.getUrl()+"/info",String.class);
+		System.out.println(res.getStatusCode().value());
+		System.out.println(res.getHeaders().get("content-Type"));
+		System.out.println(res.getBody());
+	}
+	}
+	
